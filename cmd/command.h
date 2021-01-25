1310988e9310c97a798155cadb05967dad20a5cb
dong <1101553279@qq.com>
2021-01-25 11:16:07 +0800

    prepare to refactor command, compile ok

--- cmd/command.h
+++ cmd/command.h
@@ -3,10 +3,6 @@
 
 #include "usrv.h"
 
-#define CMD_INDENT  "       "
-#define CMD_BH_SIZE 1024
-#define CMD_BM_SIZE 512
-#define CMD_BL_SIZE 256
 
 
 struct command;
@@ -18,10 +14,11 @@ struct command{
 	command_cb_t func; /*command execution function */
 };
 
+#if 0
 #define CMD_NEW(name, spec, usage, func)	\
 	static struct command __##cmd_##name = {#name, spec, usage, func};\
 	static struct command *__##pcmd_##name __attribute__((section("command"))) = &__##cmd_##name
-
+#endif
 
 int cmd_exe(struct client *cli, unsigned char *buff, unsigned int len);
 
--- plc.c
+++ plc.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 #include "plog.h"
-#include "console.h"
 #include "util.h"
 
-struct plc{
-    struct console cs;
-};
 
-struct plc plc_obj;
 
 int main(int argc, char *argv[])
 {
     /* init plog module */
     plog_init();
-    /* init console module */
-    cs_init(&(plc_obj).cs); 
 
 
     while(1)
